ca1684f78b8b7370a3498deb912b2982405aa620
Tomaz Muraus <tomaz@tomaz.me>
2018-02-26 12:50:33 +0100

    Add new action which relies on a local import.

---
+++ actions/print_version_local_import.py
@@ -0,0 +1,17 @@
+from st2actions.runners.pythonrunner import Action
+
+import local_module
+from local_module import CONSTANT_FOO
+
+__all__ = [
+    'PrintVersionAction'
+]
+
+
+class PrintVersionAction(Action):
+    def run(self):
+        version = 'v0.13.0'
+        print(local_module)
+        print(CONSTANT_FOO)
+        print(version)
+        return version
